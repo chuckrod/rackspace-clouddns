db02caa871a1895d02396dbfe81d4b82cca527b8
Rodney Richard <rrichard@qualtrax.com>
2016-02-09 21:44:17 +0000

    Made all pylint changes

--- add_record.py
+++ add_record.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python
+"""
+Script used to add Rackspace DNS Record
+"""
 
 import pyrax
 import os
 import sys
 
 def main():
+    """
+    Main function of script
+    """
     domain_name = sys.argv[1]
     host_name = sys.argv[2]
     ip_address = sys.argv[3]
 
-    pyrax.set_setting("identity_type","rackspace")
-    pyrax.set_credentials(os.environ['RACKSPACE_USERNAME'], os.environ['RACKSPACE_API_KEY'])
+    pyrax.set_setting("identity_type", "rackspace")
+    pyrax.set_credentials(os.environ['RACKSPACE_USERNAME'],
+			  os.environ['RACKSPACE_API_KEY'])
 
     domain = pyrax.cloud_dns.find(name=domain_name)
     record = ({"type": "A", "name": host_name, "data": ip_address, "ttl":300})
