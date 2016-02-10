a8761e4f0d32d653f4c0d3975184fa11e25455a2
Rodney Richard <rrichard@qualtrax.com>
2016-02-10 13:25:30 -0500

    Scripting add_record.py

--- add_record.py
+++ add_record.py
@@ -14,13 +14,15 @@ def main():
     domain_name = sys.argv[1]
     host_name = sys.argv[2]
     ip_address = sys.argv[3]
+    record_type = sys.argv[4]
+    ttl = sys.argv[5]
 
     pyrax.set_setting("identity_type", "rackspace")
     pyrax.set_credentials(os.environ['RACKSPACE_USERNAME'],
 			  os.environ['RACKSPACE_API_KEY'])
 
     domain = pyrax.cloud_dns.find(name=domain_name)
-    record = ({"type": "A", "name": host_name, "data": ip_address, "ttl":300})
+    record = ({"type": record_type, "name": host_name, "data": ip_address, "ttl":ttl})
 
     domain.add_records(record)
 
